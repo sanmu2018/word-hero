07b9d14cf93c92cbf6feabcb498061bf0b36bbb0
sanmu <huanghaisen2018@gmail.com>
2025-09-23 09:03:06 +0800

    add plan by person

--- internal/router/web_server.go
+++ internal/router/web_server.go
@@ -142,10 +142,9 @@ func (ws *WebServer) Start(port int) error {
 func (ws *WebServer) corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-		c.Header("Access-Control-Expose-Headers", "Content-Length")
-		c.Header("Access-Control-Allow-Credentials", "true")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
